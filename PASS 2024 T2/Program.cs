71396f3aa4ef62d2a502f964fd5f458331bddf88
branrile09 <93716194+branrile09@users.noreply.github.com>
2024-06-13 20:38:09 +0930

    Add project files.

---
+++ PASS 2024 T2/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace PASS_2024_T2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("RB waz here!!!");
+            Console.WriteLine("Hoang Bao was here.");
+            Console.WriteLine("CW was here. Hi");
+            Console.WriteLine("Tim was here.");
+            Console.WriteLine("Rob here sorry for the bad start");
+            Console.WriteLine("JORDAN WAZ HERE");
+
+
+
+
+            //Variable types here
+            float floatingPoint = 42.69f;
+            bool myNewBool = true;
+            byte bit = 0;
+            int i = 22;
+            string str = "YO PEEPS";
+            char myChar = 'A';
+            char interger = '1';
+            long l = 5;
+
+
+
+
+            //have fun with the variables!!!!
+            int x = 21;
+            int newVariable = 202;       // Good use of Cameil Case
+            double newVariable2 = 22.22;
+            double newFlot = 33.33;
+            int num;
+            double pi = 3.12;
+            int[] newArray = new int[5];
+            string name = "CONNOR";
+            bool isStudent = true;
+            int newFile = 0;
+            List<int> listVariable = new List<int>();
+            Nullable<int> variableName = null;
+            Console.WriteLine("My name is ROB CONNOR NOW");
+            string firstName = "Connor";
+            string activity = "Is having fun";
+            string combined = firstName + " " + activity;
+            Console.WriteLine(combined);
+
+
+            //number manipulation
+            x++;    // Addition to var x + 1
+            x--;
+            x *= 3; // x = x * 3
+            x /= 4; // x = x / 3  (lefthand side is shorthand, right is longhand) equivalent
+
+            name = "rob";
+            name = "rob" + " connor";
+            name = $"rob is {x} years old, his favourite number is pi = {pi}";
+
+            char firstChar = name[0];
+            //writing each char from name
+            foreach (char a in name)
+            {
+                Console.WriteLine(a);
+            }
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+}
